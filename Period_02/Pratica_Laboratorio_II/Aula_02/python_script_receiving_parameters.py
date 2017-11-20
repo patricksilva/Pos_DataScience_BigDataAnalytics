4667a17f6255c3914acf8cfd032acf72a4a1b95e
Patrick Alex Freitas da Silva <patrickalex@gmail.com>
2017-11-20 21:01:28 -0200

    Adds from sys import platform, prefix and version to Aula 02 - OOP - Importing modules part2

---
+++ Period_02/Pratica_Laboratorio_II/Aula_02/python_script_receiving_parameters.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/2/library/sys.html
+# https://docs.python.org/3.6/library/sys.html
+from sys import argv
+
+print(argv)
+
+from sys import platform, prefix, version
+print(platform, prefix, version)
